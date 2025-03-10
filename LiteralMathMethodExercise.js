e9e8eeb7ceec07dc2968ca07ac31af97171754f8
Boxrooll <tony.corliss@stu.barren.kyschools.us>
2025-03-10 10:24:48 -0500

    done

---
+++ LiteralMathMethodExercise.js
@@ -0,0 +1,17 @@
+let warmhugs = "Hi, I'm Olaf and I like warm hugs."
+console.log(warmhugs);
+console.log(warmhugs.replace("like","love"));
+
+let beenimpaled = "Oh, look at that. I've been impaled."
+console.log(beenimpaled.slice(14,28));
+
+let dotDotDot = "..."
+
+let skullBones = "I don't have a skull... or bones."
+
+console.log(Math.PI);
+
+let random = Math.random() * 3;
+random = Math.floor(random);random++;
+console.log(random);
+
---
+++ string.js
@@ -0,0 +1,12 @@
+
+
+console.log("Hello World");
+console.log("Hello World");
+
+
+
+console.log('THIS\'WILL WORK');
+
+console.log("MCU".length);
+
+
